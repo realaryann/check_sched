6410572415f40a2ca3e89962b099fbcc85bfb156
aryan <btera759@gmail.com>
2024-06-12 20:25:39 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import time 
+
+# contains the size of the upcoming data
+HEADER = 102400
+FORMAT = "utf-8"
+DISCONNECT = "EXIT!"
+
+def handle_client(conn, addr):
+    print("New Connection: ", addr)
+    connected = True
+    state: int = 0
+    while connected:
+        msg_sz = conn.recv(HEADER).decode(FORMAT)
+        if msg_sz:
+            state += 1
+            msg_sz = int(msg_sz)
+            msg = conn.recv(msg_sz).decode(FORMAT)
+            # Collect all data
+            if state >= 3:
+                done = False
+                file_bytes = b' '
+                while not done:
+                    data = conn.recv(HEADER)
+                    if (data[-4::] == b'END!'):
+                        done = True
+                        state=0
+                    else:
+                        file_bytes += data
+            if msg == DISCONNECT:
+                connected=False
+    conn.close()
+    
+
+def handle_server(server):
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print("Active Connections: ", threading.activeCount()-1)
+    
+def main():
+    PORT = 9999
+    SERVER = socket.gethostbyname(socket.gethostname())
+    ADDR = (SERVER, PORT)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    print("Server Starting...")
+    handle_server(server)
+
+    
+    
+
+if __name__ == "__main__":
+    main()
