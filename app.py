d66fbc8c4249ab90ea59ee73be2e3a7453539f5a
aryan <btera759@gmail.com>
2024-06-20 09:12:53 -0400

    Frontend for submit page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
+@app.route('/submit')
+def submit_page():
+	return render_template("submit.html")
 
 if __name__ == "__main__":
 	app.run()
