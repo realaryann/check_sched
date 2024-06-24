35aab7457d1b05ac60afdfe612e275ef51810fc1
aryan <btera759@gmail.com>
2024-06-24 19:48:28 -0400

    new files

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/', methods=['POST','GET'])
+def home():		
 	return render_template("index.html")
 
-@app.route('/submit')
+@app.route('/submit', methods=["POST","GET"])
 def submit_page():
 	return render_template("submit.html")
 
